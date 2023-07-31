86cc717a15339ff3fce0a6e606d6c03ccdaed40c
westwoong <westwoong>
2023-07-31 14:48:45 +0900

    특정 게시글 조회 API 연습
    @Get(':postId') 로 파라미터값을 받고
    method(@Param('postId') postId: number) 로 진행한다

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,15 +1,25 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { PostService } from '../services/post.service';
 import { CreatePostDto } from '../dto/createPost.dto';
 
 @Controller('posts')
 export class PostsController {
-  constructor(private readonly postService: PostService) {
-  }
+  constructor(private readonly postService: PostService) {}
 
   @Post()
   @HttpCode(201)
   create(@Body() postBody: CreatePostDto): object {
     return this.postService.create(postBody);
+  }
+
+  @Get()
+  @HttpCode(200)
+  getPosts() {
+    return this.postService.searchAll();
+  }
+
+  @Get(':postId')
+  getPostOne(@Param('postId') postId: number) {
+    return this.postService.findOne(postId);
   }
 }
--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -4,11 +4,36 @@ import { CreatePostDto } from '../dto/createPost.dto';
 @Injectable()
 export class PostService {
   post: CreatePostDto;
+  posts: CreatePostDto[] = [
+    {
+      id: 1,
+      title: '제목',
+      content: '내용',
+    },
+    {
+      id: 2,
+      title: '제목2',
+      content: '내용2',
+    },
+    {
+      id: 3,
+      title: '제목3',
+      content: '내용3',
+    },
+  ];
 
   constructor() {}
 
   create(postData: CreatePostDto): object {
     this.post = postData;
-    return this.post
+    return this.post;
+  }
+
+  searchAll(): CreatePostDto[] {
+    return this.posts;
+  }
+
+  findOne(postId: number): CreatePostDto | undefined {
+    return this.posts.find((post: CreatePostDto): boolean => post.id == postId);
   }
 }
