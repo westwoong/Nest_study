23ff10f98884783be0f07f33dd1522b42d62c101
westwoong <westwoong>
2023-07-31 18:14:16 +0900

    댓글 조회,삭제 API
    [endpoint]
    @Get posts/:postId/comments
    @Delete posts/:postId/comments/:commentId
    
    조회시 postId에 달린 댓글 전체 조회(Param인 postId값이 같은 요소)
    삭제시 id(commentId)기준 삭제

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Param,
+  Delete,
+  HttpCode,
+  Get,
+} from '@nestjs/common';
 import { CommentService } from '../services/comment.service';
 import { CreateCommentDto } from '../dto/createComment.dto';
 
@@ -12,5 +20,17 @@ export class CommentController {
     @Body() commentBody: CreateCommentDto,
   ) {
     return this.commentService.createComment(postId, commentBody);
+  }
+
+  @Get()
+  @HttpCode(200)
+  getComments(@Param('postId') postId: number): CreateCommentDto[] {
+    return this.commentService.searchCommentsByPostId(postId);
+  }
+
+  @Delete(':commentId')
+  @HttpCode(204)
+  delete(@Param('commentId') commentId: number) {
+    this.commentService.deleteByCommentId(commentId);
   }
 }
