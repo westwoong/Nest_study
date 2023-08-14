72970dd966acf8ede8c23135b327e40f1c4096e5
westwoong <westwoong>
2023-08-14 22:27:50 +0900

    지난 게시글 관련 코드 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PostModule } from './posts/post.module';
 import { ORMModule } from './config/ORM.module';
 import { ProductModule } from './products/product.module';
 import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [PostModule, ProductModule, UserModule, ORMModule],
+  imports: [ProductModule, UserModule, ORMModule],
 })
 export class AppModule {}
--- src/posts/dto/createPost.request.dto.ts
+++
@@ -1,4 +0,0 @@
-export class CreatePostRequestDto {
-  title: string;
-  content: string;
-}
