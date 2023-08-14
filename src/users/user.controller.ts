653f1e7da9eab9c506de985bc24a8aad30295701
westwoong <westwoong>
2023-08-15 04:36:52 +0900

    판매자 정보 조회 API 테스트 완료

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserRequestDto } from './dto/createUser.request.dto';
 
@@ -10,5 +10,11 @@ export class UserController {
   @HttpCode(201)
   signUp(@Body() userData: CreateUserRequestDto) {
     return this.userService.signUp(userData);
+  }
+
+  @Get('/search')
+  @HttpCode(200)
+  search(@Query('seller') seller: string) {
+    return this.userService.searchSeller(seller);
   }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserRequestDto } from './dto/createUser.request.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './User.entity';
 import { CreateUserResponseDto } from './dto/createUser.response.dto';
+import { SearchUserResponseDto } from './dto/searchUser.response.dto';
 
 @Injectable()
 export class UserService {
@@ -27,5 +28,33 @@ export class UserService {
     const savedUser = await this.userRepository.save(user);
 
     return new CreateUserResponseDto(savedUser).welcome();
+  }
+
+  async searchSeller(sellerName: string) {
+    // const seller = await this.userRepository.findOne({
+    //   where: { name: sellerName },
+    //   relations: ['products'],
+    // });
+    const seller = await this.userRepository
+      .createQueryBuilder('user')
+      .leftJoinAndSelect('user.products', 'products')
+      .where('user.name = :sellerName', { sellerName })
+      .getOne();
+
+    if (!seller) {
+      throw new NotFoundException('해당 셀러 없음');
+    }
+
+    const response = {
+      name: seller.name,
+      companyName: seller.companyName,
+      productList: seller.products.map((product) => ({
+        productName: product.name,
+        price: product.price,
+      })),
+    };
+
+    console.log(response);
+    return new SearchUserResponseDto(response);
   }
 }
