ad04c88a07390c56650e66779dcb817e9affd769
westwoong <westwoong>
2023-08-21 21:48:20 +0900

    TypeORM Transaction 라이브러리 설치 및 세팅

--- src/config/ORM.module.ts
+++ src/config/ORM.module.ts
@@ -1,20 +1,32 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'woong123',
-      database: 'nest',
-      autoLoadEntities: true, // 자동으로 엔티티 관계를 인식해라
-      synchronize: true,
-      logging: true, // sequelize 로깅이랑 동일
-      namingStrategy: new SnakeNamingStrategy(), // 코드에서 카멜케이스로된 테이블명을 DB에서 스네이크케이스로 변경해주는것
+    TypeOrmModule.forRootAsync({
+      useFactory() {
+        return {
+          type: 'mysql',
+          host: '127.0.0.1',
+          port: 3306,
+          username: 'root',
+          password: 'woong123',
+          database: 'nest',
+          autoLoadEntities: true, // 자동으로 엔티티 관계를 인식해라
+          synchronize: true,
+          logging: true, // sequelize 로깅이랑 동일
+          namingStrategy: new SnakeNamingStrategy(), // 코드에서 카멜케이스로된 테이블명을 DB에서 스네이크케이스로 변경해주는것
+        };
+      },
+      async dataSourceFactory(options) {
+        if (!options) {
+          throw new Error('옵션 설정 에러');
+        }
+        return addTransactionalDataSource(new DataSource(options));
+      },
     }),
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { initializeTransactionalContext } from 'typeorm-transactional';
 
 async function startServer() {
+  initializeTransactionalContext();
+  
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
