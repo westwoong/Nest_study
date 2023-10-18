8e798a49c18a8e42ccfe121a8c98728cb9cf2468
westwoong <westwoong>
2023-10-18 21:28:58 +0900

    Test : 커밋 테스트

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { initializeTransactionalContext } from 'typeorm-transactional';
 
 async function startServer() {
   initializeTransactionalContext();
-  
+
+  console.log('테스트 입니다.');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
